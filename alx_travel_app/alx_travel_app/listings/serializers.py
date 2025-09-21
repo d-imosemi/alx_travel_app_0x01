bdb4b38326557f8664fb4925169c5714115e7a82
Imosemi <83042447+d-imosemi@users.noreply.github.com>
2025-09-22 00:18:32 +0300

    Update serializers.py

--- alx_travel_app/alx_travel_app/listings/serializers.py
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -1,1 +1,24 @@
 
+from rest_framework import serializers
+from .models import Listing, Booking
+
+class ListingSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Listing model.
+    """
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = ['id', 'title', 'description', 'price', 'city', 'country', 'address', 'owner_username', 'created_at']
+
+class BookingSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Booking model.
+    """
+    listing_title = serializers.CharField(source='listing.title', read_only=True)
+    guest_username = serializers.CharField(source='guest.username', read_only=True)
+
+    class Meta:
+        model = Booking
+        fields = ['id', 'listing_title', 'guest_username', 'check_in_date', 'check_out_date', 'total_price', 'created_at']
